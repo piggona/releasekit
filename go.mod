2fa2b80b0d54cacc716150493d8c606bb270ccd3
piggona <2803302887@qq.com>
2020-07-29 21:31:59 +0800

    feature:two step commit.add:go mod tidy check

--- go.mod
+++ go.mod
@@ -5,6 +5,4 @@ go 1.14
 require (
 	github.com/bitly/go-simplejson v0.5.0
 	github.com/go-git/go-git/v5 v5.1.0
-	github.com/libgit2/git2go v0.28.4 // indirect
-	github.com/tidwall/gjson v1.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"regexp"
 	"strconv"
 	"strings"
@@ -24,11 +26,12 @@ import (
 
 func main() {
 	var (
-		err        error
-		conf       *simplejson.Json
-		newVersion string
+		err  error
+		conf *simplejson.Json
+		ver  string
 	)
 	confpath := "./config.json"
+	path := "./repo/"
 	conf, err = utils.ReadJSON(confpath)
 	if err != nil {
 		log.Fatalf("read config file error: %s\n", err)
@@ -37,29 +40,80 @@ func main() {
 	email, _ := conf.Get("email").String()
 	token, _ := conf.Get("accesstoken").String()
 	repo, _ := conf.Get("git_repo").String()
+	fingerprint, _ := conf.Get("gpg_fingerprint").String()
+
+	exists, err := utils.PathExists(path)
+	if !exists || err != nil {
+		err := os.Mkdir(path, os.ModePerm)
+		if err != nil {
+			log.Fatalf("create new directory error: %s\n", err)
+		}
+	}
+	err = os.Chdir(path)
+	if err != nil {
+		log.Printf("change directory error: %s\n", err)
+		return
+	}
 
 	r := InitRepo(username, token, repo)
 
-	newVersion, err = ModifyChangelog("CHANGELOG.md", 2)
+	// 将旧的版本信息改为已发布
+	ver, err = ModifyChangelog("CHANGELOG.md")
 	if err != nil {
 		log.Printf("modify changelog error: %s\n", err)
 		return
 	}
-
+	log.Println(ver)
+	err = RunTidy("./")
+	if err != nil {
+		log.Printf("run go mod tidy error: %s\n", err)
+		return
+	}
+	// 然后commit，打tag并push
 	sig := &object.Signature{
 		Name:  username,
 		Email: email,
 		When:  time.Now(),
 	}
-	err = CommitRepo(r, sig)
+	err = CommitRepo(r, sig, "release version "+ver)
 	if err != nil {
 		log.Printf("commit repo error: %s\n", err)
 		return
 	}
 	utils.CheckIfError(err)
-	err = PushRepo(r, newVersion, username, token, sig)
+	err = PushRepo(r, ver, username, token, sig)
 	if err != nil {
 		log.Printf("push repo error: %s\n", err)
+		return
+	}
+
+	// 执行Release
+	err = ReleaseExec("./", token, fingerprint)
+	if err != nil {
+		log.Printf("release execution error: %s\n", err)
+		return
+	}
+	err = os.RemoveAll("./dist")
+	if err != nil {
+		log.Printf("remove dist error: %s\n", err)
+		return
+	}
+	// 生成新的CHANGELOG UNRELEASED
+	err = SetNewVersion("CHANGELOG.md", ver, 2)
+	if err != nil {
+		log.Printf("set new version changelog error: %s\n", err)
+		return
+	}
+	// push上去
+	err = CommitRepo(r, sig, "new version changelog")
+	if err != nil {
+		log.Printf("commit repo error: %s\n", err)
+		return
+	}
+	utils.CheckIfError(err)
+	err = SimplePush(r, username, token)
+	if err != nil {
+		log.Printf("simple push error: %s\n", err)
 		return
 	}
 
@@ -67,22 +121,11 @@ func main() {
 
 func InitRepo(username, token, repo string) (r *git.Repository) {
 	var (
-		err    error
-		exists bool
+		err error
 	)
-	var path = "./repo"
-
-	exists, err = utils.PathExists(path)
-	if !exists || err != nil {
-		err := os.Mkdir(path, os.ModePerm)
-		if err != nil {
-			log.Fatalf("create new directory error: %s\n", err)
-		}
-	}
-
-	files, _ := ioutil.ReadDir(path)
+	files, _ := ioutil.ReadDir(".")
 	if len(files) == 0 {
-		r, err = git.PlainClone(path, false, &git.CloneOptions{
+		r, err = git.PlainClone("./", false, &git.CloneOptions{
 			Auth: &http.BasicAuth{
 				Username: username,
 				Password: token,
@@ -93,7 +136,7 @@ func InitRepo(username, token, repo string) (r *git.Repository) {
 		utils.CheckIfError(err)
 	} else {
 		var w *git.Worktree
-		r, err = git.PlainOpen(path)
+		r, err = git.PlainOpen("./")
 		utils.CheckIfError(err)
 		w, err = r.Worktree()
 		err = w.Pull(&git.PullOptions{RemoteName: "origin"})
@@ -104,7 +147,7 @@ func InitRepo(username, token, repo string) (r *git.Repository) {
 	return r
 }
 
-func CommitRepo(r *git.Repository, sig *object.Signature) error {
+func CommitRepo(r *git.Repository, sig *object.Signature, comment string) error {
 	var (
 		w          *git.Worktree
 		commitHash plumbing.Hash
@@ -114,39 +157,33 @@ func CommitRepo(r *git.Repository, sig *object.Signature) error {
 	w, err = r.Worktree()
 	if err != nil {
 		log.Printf("get worktree error: %s\n", err)
-		return fmt.Errorf("get worktree error: %s", err)
-	}
-	path := "./repo/"
-	err = os.Chdir(path)
-	if err != nil {
-		log.Printf("change directory error: %s\n", err)
-		return fmt.Errorf("change directory error: %s", err)
+		return err
 	}
 	_, err = w.Add(".")
 	if err != nil {
 		log.Printf("add files to repository error: %s\n", err)
-		return fmt.Errorf("add files to repository error: %s", err)
+		return err
 	}
 
 	status, err = w.Status()
 	if err != nil {
 		log.Printf("get status error: %s\n", err)
-		return fmt.Errorf("get status error: %s", err)
+		return err
 	}
 	fmt.Println(status)
 
-	commitHash, err = w.Commit("new release version", &git.CommitOptions{
+	commitHash, err = w.Commit(comment, &git.CommitOptions{
 		Author: sig,
 	})
 	if err != nil {
 		log.Printf("commit error: %s\n", err)
-		return fmt.Errorf("commit error: %s", err)
+		return err
 	}
 	utils.Info("git show -s")
 	obj, err := r.CommitObject(commitHash)
 	if err != nil {
 		log.Printf("get commit object error: %s\n", err)
-		return fmt.Errorf("get commit object error: %s", err)
+		return err
 	}
 	fmt.Println(obj)
 	return nil
@@ -223,8 +260,32 @@ func pushTags(r *git.Repository, username, token string) error {
 	return nil
 }
 
+func SimplePush(r *git.Repository, username, token string) error {
+	auth := &http.BasicAuth{
+		Username: username,
+		Password: token,
+	}
+
+	po := &git.PushOptions{
+		RemoteName: "origin",
+		Progress:   os.Stdout,
+		Auth:       auth,
+	}
+	utils.Info("git push --tags")
+	err := r.Push(po)
+	if err != nil {
+		if err == git.NoErrAlreadyUpToDate {
+			log.Print("origin remote was up to date,no push done")
+			return nil
+		}
+		log.Printf("push to remote origin error: %s", err)
+		return err
+	}
+	return nil
+}
+
 func PushRepo(r *git.Repository, tag, username, token string, sig *object.Signature) error {
-	_, err := setTag(r, tag, sig)
+	_, err := setTag(r, "v"+tag, sig)
 	if err != nil {
 		return err
 	}
@@ -235,7 +296,7 @@ func PushRepo(r *git.Repository, tag, username, token string, sig *object.Signat
 	return nil
 }
 
-func ModifyChangelog(filename string, mode int) (string, error) {
+func ModifyChangelog(filename string) (string, error) {
 	var (
 		file       *os.File
 		wfile      *os.File
@@ -247,31 +308,30 @@ func ModifyChangelog(filename string, mode int) (string, error) {
 		newVersion string
 	)
 	// 先判断有没有这个文件
-	var path = "./repo/" + filename
-	exists, err = utils.PathExists(path)
+	exists, err = utils.PathExists(filename)
 	if !exists || err != nil {
 		exists = false
-		file, err = os.Create(path)
+		file, err = os.Create(filename)
 		if err != nil {
-			log.Printf("create file error %s: %s", path, err)
-			return "", fmt.Errorf("create file error %s: %s", path, err)
+			log.Printf("create file error %s: %s", filename, err)
+			return "", fmt.Errorf("create file error %s: %s", filename, err)
 		}
 	}
 
 	// 然后取以##开头的行，取数字，将后面的Unreleased改为今日日期
 	if file == nil {
-		file, err = os.Open(path)
+		file, err = os.Open(filename)
 		if err != nil {
-			log.Printf("open file error %s: %s", path, err)
-			return "", fmt.Errorf("open file error %s: %s", path, err)
+			log.Printf("open file error %s: %s", filename, err)
+			return "", fmt.Errorf("open file error %s: %s", filename, err)
 		}
 		defer file.Close()
 	}
-	wfile, err = os.Create(path + ".tmp")
+	wfile, err = os.Create(filename + ".tmp")
 	defer wfile.Close()
 	if err != nil {
-		log.Printf("create temp file error %s: %s", path, err)
-		return "", fmt.Errorf("create temp file error %s: %s", path, err)
+		log.Printf("create temp file error %s: %s", filename, err)
+		return "", fmt.Errorf("create temp file error %s: %s", filename, err)
 	}
 	reader = bufio.NewReader(file)
 	writer = bufio.NewWriter(wfile)
@@ -283,7 +343,7 @@ func ModifyChangelog(filename string, mode int) (string, error) {
 
 	if !exists {
 		var str string
-		str, newVersion = LogGenerator("", 0)
+		str, newVersion = LogGenerator("")
 		fmt.Fprintln(writer, str)
 	} else {
 		for {
@@ -292,13 +352,13 @@ func ModifyChangelog(filename string, mode int) (string, error) {
 				break
 			}
 			if err != nil {
-				log.Printf("read line in file error %s: %s", path, err)
-				return "", fmt.Errorf("read line in file error %s: %s", path, err)
+				log.Printf("read line in file error %s: %s", filename, err)
+				return "", fmt.Errorf("read line in file error %s: %s", filename, err)
 			}
 			str := *(*string)(unsafe.Pointer(&bfRead))
 			if regex.MatchString(str) {
 				// 把str修改一下
-				str, newVersion = LogGenerator(str, mode)
+				str, newVersion = LogGenerator(str)
 			}
 			// 将该行写入文件
 			fmt.Fprintln(writer, str)
@@ -306,23 +366,23 @@ func ModifyChangelog(filename string, mode int) (string, error) {
 	}
 	err = writer.Flush()
 	if err != nil {
-		log.Printf("writer flush error %s: %s", path+"tmp", err)
-		return "", fmt.Errorf("writer flush error %s: %s", path+"tmp", err)
+		log.Printf("writer flush error %s: %s", filename+"tmp", err)
+		return "", fmt.Errorf("writer flush error %s: %s", filename+"tmp", err)
 	}
-	err = os.Remove(path)
+	err = os.Remove(filename)
 	if err != nil {
-		log.Printf("remove file error %s: %s", path, err)
-		return "", fmt.Errorf("remove file error %s: %s", path, err)
+		log.Printf("remove file error %s: %s", filename, err)
+		return "", fmt.Errorf("remove file error %s: %s", filename, err)
 	}
-	err = os.Rename(path+".tmp", path)
+	err = os.Rename(filename+".tmp", filename)
 	if err != nil {
-		log.Printf("rename file error %s: %s", path+".tmp", err)
-		return "", fmt.Errorf("rename file error %s: %s", path+".tmp", err)
+		log.Printf("rename file error %s: %s", filename+".tmp", err)
+		return "", fmt.Errorf("rename file error %s: %s", filename+".tmp", err)
 	}
 	return newVersion, nil
 }
 
-func LogGenerator(version string, mode int) (string, string) {
+func LogGenerator(version string) (string, string) {
 	var newVersion string
 	var newDate string
 	if len(version) == 0 {
@@ -331,12 +391,78 @@ func LogGenerator(version string, mode int) (string, string) {
 	}
 	reg, _ := regexp.Compile("[0-9]*\\.[0-9]*\\.[0-9]*")
 	ver := reg.Find([]byte(version))
+	now := time.Now()
+	newDate = fmt.Sprintf("%s %s, %s", now.Month().String(), strconv.Itoa(now.Day()), strconv.Itoa(now.Year()))
+
+	return fmt.Sprintf("## %s (%s)", string(ver), newDate), string(ver)
+}
+
+func SetNewVersion(filename string, ver string, mode int) error {
 	strs := strings.Split(*(*string)(unsafe.Pointer(&ver)), ".")
 	n, _ := strconv.Atoi(strs[mode])
 	n++
 	strs[mode] = strconv.Itoa(n)
-	newVersion = strings.Join(strs, ".")
-	now := time.Now()
-	newDate = fmt.Sprintf("%s %s, %s", now.Month().String(), strconv.Itoa(now.Day()), strconv.Itoa(now.Year()))
-	return fmt.Sprintf("## %s (Unreleased)\n## %s (%s)", newVersion, string(ver), newDate), newVersion
+	newVersion := strings.Join(strs, ".")
+
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Printf("open file %s error: %s\n", filename, err)
+		return err
+	}
+	defer f.Close()
+	contents, _ := ioutil.ReadAll(f)
+	newContents := fmt.Sprintf("## %s (Unreleased)\n", newVersion) + *(*string)(unsafe.Pointer(&contents))
+	wf, err := os.OpenFile(filename, os.O_WRONLY|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Printf("open write file %s error: %s\n", filename, err)
+		return err
+	}
+	num, err := wf.WriteString(newContents)
+	if err != nil || num < 1 {
+		log.Printf("write file error: %v\n,wrote %d bytes", err, num)
+		return fmt.Errorf("write error %s", err)
+	}
+	return nil
+}
+
+func ReleaseExec(workdir string, token string, fingerprint string) error {
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	_, err := os.Stat(workdir + "dist/")
+	if err == nil {
+		err = os.RemoveAll(workdir + "dist/")
+		if err != nil {
+			log.Printf("remove dist error: %s\n", err)
+			return err
+		}
+	}
+	cmd := exec.Command("goreleaser", "release", "--rm-dist")
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	cmd.Env = os.Environ()
+	cmd.Env = append(cmd.Env, "GITHUB_TOKEN="+token, "GPG_FINGERPRINT="+fingerprint)
+	err = cmd.Run()
+	if err != nil {
+		log.Printf("cmd goreleaser error: %s\nstderr log: %s\n", err, stderr.String())
+		return err
+	}
+	utils.Info("command goreleaser release --rm-dist output:\n")
+	log.Printf("%s\n", out.String())
+	return nil
+}
+
+func RunTidy(workdir string) error {
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command("go", "mod", "tidy")
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("cmd go mod tidy error: %s\nstderr log: %s\n", err, stderr.String())
+		return err
+	}
+	utils.Info("command go mod tidy:\n")
+	log.Printf("%s\n", out.String())
+	return nil
 }
